6d7443cc99dba727f42e135d4824c826e67ee8c4
Milindkumar Shah <Milindkumar@Milinds-Macbook-Pro.local>
2017-01-30 22:23:27 -0800

    setup of project

---
+++ server.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/', express.static(path.join(__dirname, 'src/client/')));
+app.use(function(req, res, next) {
+    // Set permissive CORS header - this allows this server to be used only as
+    // an API server in conjunction with something like webpack-dev-server.
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    // Disable caching so we'll always get the latest comments.
+    res.setHeader('Cache-Control', 'no-cache');
+    next();
+});
+
+var port = normalizePort(process.env.PORT || '1234');
+app.set('port', port);
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+}
+
+app.listen(app.get('port'), function() {
+  console.log('Server started at: http://localhost:' + app.get('port') + '/');
+});
