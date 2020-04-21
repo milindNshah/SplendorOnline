11c7ad72218690e68c35d3abee06281720887e55
milindNshah <milinds.905@gmail.com>
2020-04-20 23:05:46 -0700

    Added typescript

--- server.ts
+++ server.ts
@@ -1,15 +1,15 @@
-const bodyParser = require('body-parser');
-const express = require('express');
-const opn = require('opn');
-const path = require('path');
-const socketIO = require('socket.io');
+import bodyParser from 'body-parser';
+import express from 'express';
+import open from 'open';
+import path from 'path';
+import socketIO from 'socket.io';
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/', express.static(path.join(__dirname, 'src/client/')));
-app.use(function(req, res, next) {
+app.use(function (_req, res, next) {
     // Set permissive CORS header - this allows this server to be used only as
     // an API server in conjunction with something like webpack-dev-server.
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -21,28 +21,27 @@ app.use(function(req, res, next) {
 let port = normalizePort(process.env.PORT || '1234');
 app.set('port', port);
 
-function normalizePort(val) {
-  const port = parseInt(val, 10);
-  if (isNaN(port)) {
-    return val;
-  }
-  if (port >= 0) {
-    return port;
-  }
-  return false;
+function normalizePort(val: any): any {
+    const port = parseInt(val, 10);
+    if (isNaN(port)) {
+        return val;
+    }
+    if (port >= 0) {
+        return port;
+    }
+    return false;
 }
 
-opn('http://localhost:'+app.get('port'));
+open('http://localhost:' + app.get('port'));
 const server = app.listen(app.get('port'), function () {
-  console.log('Server started at: http://localhost:' + app.get('port') + '/');
+    console.log('Server started at: http://localhost:' + app.get('port') + '/');
 });
 const io = socketIO(server);
 
-// Add the WebSocket handlers
-io.on('connection', function(socket) {
-  "io connected";
+io.on('connection', function (_socket) {
+    "io connected";
 });
 
-setInterval(function() {
-  io.sockets.emit('message', 'hi!');
+setInterval(function () {
+    io.sockets.emit('message', 'hi!');
 }, 1000);
