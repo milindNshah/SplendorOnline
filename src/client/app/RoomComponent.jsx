e63b133a0ca359c794f71671fd006af38b810eb1
milindNshah <milinds.905@gmail.com>
2020-04-22 12:52:25 -0700

    Generate the player table

--- src/client/app/RoomComponent.jsx
+++ src/client/app/RoomComponent.jsx
@@ -4,17 +4,19 @@ import Button from 'react-bootstrap/Button';
 import { socket } from './socket';
 
 class RoomComponent extends React.Component {
-  constructor(props) {
+  constructor (props) {
     super(props)
     this.state = {
       playerID: '',
       roomCode: '',
-      playersInfo: {},
+      playersInfo: [{}],
       player: {},
     }
     this.socket = socket;
     this.onRoomUpdate = this.onRoomUpdate.bind(this);
     this.setClientPlayerID = this.setClientPlayerID.bind(this);
+    this.createPlayerTable = this.createPlayerTable.bind(this);
+    this.createPlayerRow = this.createPlayerRow.bind(this);
   }
 
   componentDidMount() {
@@ -22,36 +24,70 @@ class RoomComponent extends React.Component {
     this.socket.on('clientPlayerID', this.setClientPlayerID);
   }
 
-  onRoomUpdate({room, players}) {
-    console.log(players)
+  onRoomUpdate({ room, players }) {
     const playersInfo = new Map();
     players.forEach((someone) => {
       playersInfo.set(someone.id, someone);
     })
-
     const currentPlayer = playersInfo.get(this.state.playerID)
-    console.log("client: ", currentPlayer);
 
     this.setState({
       roomCode: room.code,
-      // playersInfo: playersInfo,
-      // player: currentPlayer,
+      playersInfo: playersInfo,
+      player: currentPlayer,
     });
   }
 
   setClientPlayerID(playerID) {
-    console.log(playerID);
     this.setState({
       playerID: playerID,
     });
   }
 
+  createPlayerTable() {
+    const size = this.state.playersInfo.size;
+    if (size === null || size === undefined || size <= 0) {
+      return;
+    }
+
+    const rows = Array.from(this.state.playersInfo.values()).map((player) => {
+      return this.createPlayerRow(player);
+    })
+
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col">Host</div>
+          <div className="col">Name</div>
+          <div className="col">Ready</div>
+        </div>
+        {rows}
+      </div>
+    );
+  }
+
+  createPlayerRow(player) {
+    return (
+      <div className="row" key={player.id}>
+        <div className="col">{player.isHost}</div>
+        <div className="col">{player.user.name}</div>
+        <div className="col">{player.isReady}</div>
+      </div>
+    )
+  }
+
   render() {
     return (
       <div>
-        <p>Room Code</p>
-        <Button variant="outline-dark">{this.state.roomCode}</Button>
-        <p>PlayerID: {this.state.playerID}</p>
+        <div>
+          <p>Room Code</p>
+          <Button variant="outline-dark">{this.state.roomCode}</Button>
+          <p>PlayerID: {this.state.playerID}</p>
+        </div>
+        <div>
+          <h1>Player Info</h1>
+          {this.createPlayerTable()}
+        </div>
       </div>
     );
   }
