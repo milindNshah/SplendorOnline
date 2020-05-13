fe0318136ba8c1ed26ec5087ef529a5a5fa63c44
milindNshah <milinds.905@gmail.com>
2020-05-13 11:41:10 -0700

    Change modal width

--- src/client/app/styledcomponents/modal-container.jsx
+++ src/client/app/styledcomponents/modal-container.jsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 const ModalContainer = styled.div`
-  width: ${ props => `${props.width + (2 * props.theme.modal.padding)}rem`};
+  width: ${ props => `${props.width}rem`};
   max-height: 80vh;
   padding: ${ props => `${props.theme.modal.padding}rem`};
   background-color: ${ props => props.theme.color.white};
